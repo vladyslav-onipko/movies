db4f1b6d2e49dde48c8b61b4c208a99fa08d0c4a
vladonipko <vladonipko@o-s-i.org>
2023-10-31 15:18:28 +0200

    fix publicPth

--- src/components/Movies/MovieItem.vue
+++ src/components/Movies/MovieItem.vue
@@ -21,15 +21,12 @@
 <script>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
-import { useRoute } from 'vue-router';
 
 export default {
     props: ['id', 'img', 'title', 'description'],
     setup(props) {
         const store = useStore();
-        const route = useRoute();
-        
-        const detailLink = computed(() =>  route.path + props.id);
+        const detailLink = computed(() => 'movies' + '/' + props.id);
 
         const removeMovie = async () => {
             try {
